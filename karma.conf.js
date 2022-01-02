9cab83ea55c19c220ba9f0f5b8f667345fad83ff
AnudeepK23 <90596066+AnudeepK23@users.noreply.github.com>
2022-01-02 19:47:15 +0530

    bank-app

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/service-app'),
+      dir: require('path').join(__dirname, './coverage/bank-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
