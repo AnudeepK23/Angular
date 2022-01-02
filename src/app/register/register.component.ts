93d2f720c0f20dc8bbd5ae36a9dece72d6aebbfe
AnudeepK23 <90596066+AnudeepK23@users.noreply.github.com>
2022-01-02 19:48:41 +0530

    newbank
    Running project

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Bank } from 'src/bank';
+import { BankService } from '../bank.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  bn:Bank=new Bank();
+
+  constructor(private bank:BankService) { }
 
   ngOnInit(): void {
   }
 
+
+
+save(obj:any){
+  this.bn.id=obj.id;
+  this.bn.fullName=obj.fullName;
+  this.bn.email=obj.email;
+  this.bn.contact=obj.contact;
+  this.bn.account=obj.account;
+  this.bn.password=obj.password;
+  this.bn.confirmpassword=obj.confirmpassword;
+
+if(this.bn !== null)
+{
+ 
+  this.bank.saveBan(this.bn).subscribe( (res) =>{
+    alert("Registered Succesfully")
+    console.log(res);
+  },err=>{
+  
+    alert("Something went wrong");
+  });
+
+}
+
+ 
+
+}
 }
