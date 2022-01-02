01ca67248aeccfc8888c16d611c0cc030e9c3d6e
AnudeepK23 <90596066+AnudeepK23@users.noreply.github.com>
2022-01-02 20:02:09 +0530

    router-app

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pipes-app'),
+      dir: require('path').join(__dirname, './coverage/router-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
